fe5a600d980e009952666286f74fa676b01cd4f7
idefav <wzs2009_hi@126.com>
2016-07-04 23:08:28 +0800

    修复bug

---
+++ Idefav.Utility/DBType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Idefav.Utility
+{
+    /// <summary>
+    /// 数据库类型
+    /// </summary>
+   public enum DBType
+    {
+        SQLServer=1,
+        MySQL=2,
+        OleDb=3,
+        Oracle=4,
+        SQLite=5,
+    }
+
+    /// <summary>
+    /// 排序方向
+    /// </summary>
+    public enum OrderDirection
+    {
+        DESC=1,ASC=-1
+    }
+}
