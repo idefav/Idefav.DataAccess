5f01dc8f7495f2ebcd2357328c7e8be7ed15e26c
idefav <wzs2009_hi@126.com>
2016-09-06 16:41:30 +0800

    测试

--- Idefav.Test/OracleTest.cs
+++ Idefav.Test/OracleTest.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Idefav.DbObjects.Oracle;
+using Idefav.IDAL;
+using Idefav.Utility;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Idefav.Test
@@ -20,6 +22,15 @@ namespace Idefav.Test
             int count = 0;
             Assert.AreEqual(db.QueryPageTable("select t.* from fundadmin.cfg_advert t", 1, 10, "eid", "*").Rows.Count>0,true);
              
+        }
+
+        public void TestGetPageDataTable_2()
+        {
+            IDbObject db = DbFactory.DBOMaker.CreateDbObj(DBType.Oracle, ConnStr);
+            db.DbConnectStr = ConnStr;
+            int count = 0;
+            db.QueryPageTable("", 1, 20, out count, "eid", "*", new {});
+            Assert.AreEqual(db.QueryPageTable("select t.* from fundadmin.cfg_advert t", 1, 10, "eid", "*").Rows.Count > 0, true);
         }
     }
 }
